3a39ca6c99e8c2f95b0cdc33b2c4dd62d53cee79
Anil Kumar Singh <singhanil2005anil@gmail.com>
2026-07-06 22:41:38 +0530

    Time: 35 ms (27.12%), Space: 47.6 MB (80.16%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,14 +1,10 @@
-import java.util.*;
-
 class Triplet implements Comparable<Triplet> {
     int ele;
     int dist;
-
     Triplet(int ele, int dist) {
         this.ele = ele;
         this.dist = dist;
     }
-
     @Override
     public int compareTo(Triplet other) {
         // Sort by distance, then by value
@@ -18,23 +14,20 @@ class Triplet implements Comparable<Triplet> {
         return this.dist - other.dist;
     }
 }
-
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         PriorityQueue<Triplet> pq = new PriorityQueue<>();
 
-        // Step 1: Build heap with distance from x
+        //  Build heap with distance from x
         for (int num : arr) {
             pq.add(new Triplet(num, Math.abs(num - x)));
         }
-
-        // Step 2: Extract k closest
+        //  Extract k closest
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < k; i++) {
             ans.add(pq.poll().ele);
         }
-
-        // Step 3: Sort result before returning
+        // Sort result before returning
         Collections.sort(ans);
         return ans;
     }
