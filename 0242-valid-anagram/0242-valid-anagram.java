d0e0f3334805f0fa2fbf42b8ee7df22be27a3cad
Anil Kumar Singh <singhanil2005anil@gmail.com>
2026-07-09 00:58:39 +0530

    Time: 14 ms (26.51%), Space: 46.1 MB (49.48%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public HashMap<Character, Integer> makeFreqMap(String str){
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for(int i = 0; i < str.length(); i++) {
+            Character ch =  str.charAt(i);
+            mp.put(ch, mp.getOrDefault(ch,0)+1);
+        }
+        return mp;
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> mp1 = makeFreqMap(s);
+        HashMap<Character, Integer> mp2 = makeFreqMap(t);
+        return mp1.equals(mp2);
+    }
+        
+}
